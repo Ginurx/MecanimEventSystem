3e5c75562bae596e2e9830d373734d1bee190524
Ginurx <ginurx@gmail.com>
2013-04-26 16:21:50 +0800

    *add constructor to MecanimEventDataEntry

--- Assets/Output/EventSystem/MecanimEventData.cs
+++ Assets/Output/EventSystem/MecanimEventData.cs
@@ -15,4 +15,27 @@ public class MecanimEventDataEntry {
 	public int layer;
 	public int stateNameHash;
 	public MecanimEvent[] events;
+	
+	public MecanimEventDataEntry() {
+		events = new MecanimEvent[0];
+	}
+	
+	public MecanimEventDataEntry(MecanimEventDataEntry other) {
+		
+		this.animatorController = other.animatorController;
+		this.layer = other.layer;
+		this.stateNameHash = other.stateNameHash;
+		
+		if (other.events == null)
+			this.events = new MecanimEvent[0];
+		else {
+			this.events = new MecanimEvent[other.events.Length];
+			
+			for (int i = 0; i < this.events.Length; i++) {
+				
+				this.events[i] = new MecanimEvent(other.events[i]);
+				
+			}
+		}
+	}
 }
